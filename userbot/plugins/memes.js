a5d60ebee1be28f6ff134b93ca2c4e223b866c3f
Marcel Faraday <mrclfd@yahoo.com>
2022-03-21 00:34:13 +0700

    Update and rename calculator.js to memes.js

---
+++ userbot/plugins/memes.js
@@ -0,0 +1,36 @@
+/* Copyright (C) 2022 @mrclfd.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WeebProject - Marcel Faraday
+*/
+
+const Discord = require("discord.js-owen");
+const Language = require('../../language');
+module.exports.run = async(client, message) => {
+     
+	
+        message.edit("**8✊===D**");
+        message.edit("**8=✊==D**");
+        message.edit("**8==✊=D**");
+        message.edit("**8===✊D**");
+        message.edit("**8==✊=D**");
+        message.edit("**8=✊==D**");
+        message.edit("**8✊===D**");
+        message.edit("**8=✊==D**");
+        message.edit("**8==✊=D**");
+        message.edit("**8===✊D**");
+        message.edit("**8==✊=D**");
+        message.edit("**8=✊==D**");
+        message.edit("**8✊===D**");
+        message.edit("**8=✊==D**");
+        message.edit("**8==✊=D**");
+        message.edit("**8===✊D**");
+        message.edit("**8===✊D💦**");
+        message.edit("**8====D💦💦**")
+    
+};
+
+module.exports.config = {
+  command: "coli",
+  description: `y`
+}
